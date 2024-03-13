52db7afcf70ff45759a030266f8cab44488e6cf8
Ankit Kaushik <klatte09>
2024-03-13 11:27:10 +0530

    merge conflict resolved

--- versions.tf
+++ versions.tf
@@ -1,7 +1,4 @@
 terraform {
-<<<<<<< HEAD
-  required_version = ">= 1.3.0"
-=======
   required_version = ">= 1.3"
   required_providers {
     azurerm = {
@@ -13,5 +10,4 @@ terraform {
       version = "~> 1.2, >= 1.2.22"
     }
   }
->>>>>>> b1b7f352d1a813b3aee03a72a16d8f387a14e16e
 }
